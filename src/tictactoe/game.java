18a40aeac17993355d79f218b7605b1bac474bd5
Evan Dhillon <evandhillon01@gmail.com>
2018-08-31 15:22:49 -0500

    bug fixes

--- src/tictactoe/game.java
+++ src/tictactoe/game.java
@@ -1,78 +1,104 @@
 package tictactoe;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 //contains all the win checks / methods
 // 
+
 public class game {
+	
 	int turn = 1;
-	board myBoard = new board();
+	boolean win;
+	board myBoard = new board(4);
 	
 	public void play() {
-		
-		switch(turn) {
-		case 1:
-			
-			if(checkWin()) {
+		while (win != true) {
+			switch(turn) {
+			case 1:
 				
-				//call board.win(turn)
-			}
-			turn = 2;
-			break;
-		case 2:
-			
-			if(checkWin()) {
+				if(checkWin()) {
+					
+					//call pieces.win(turn)
+				}
+				turn = 2;
+				break;
+			case 2:
 				
-				//call board.win(turn)
-			}
-			turn = 3;
-			break;
-		case 3:
-			
-			if(checkWin()) {
+				if(checkWin()) {
+					
+					//call pieces.win(turn)
+				}
+				turn = 3;
+				break;
+			case 3:
 				
-				//call board.win(turn)
+				if(checkWin()) {
+					
+					//call pieces.win(turn)
+				}
+				turn = 1;
+				break;
 			}
-			turn = 1;
-			break;
 		}
+		
 		
 
 		return; 
 	}
 	
-	public static boolean checkWin() {
-		for(int i = 0; i < myBoard.board.length; i++) {
-			for(int j = 0; j < myBoard.board[0].length; i++) {
+	public void saveGame() throws IOException {
+		String fileName = "savedgames.txt";
+		String fileStr = "";
+		for(int i = 0; i < myBoard.pieces[0].length; i++) {
+			for(int j = 0; j < myBoard.pieces.length; j++) {
+				String numStr = myBoard.pieces[i][j].player + ",";
+				fileStr = fileStr + numStr;
+			}
+			fileStr = fileStr + "\n";
+		}
+		BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+		writer.append(fileStr);	     
+		writer.close();
+	}
+	
+	public void loadGame() {
+		
+	}
+	
+	public boolean checkWin() {
+		for(int i = 0; i < myBoard.pieces.length; i++) {
+			for(int j = 0; j < myBoard.pieces[0].length; i++) {
 				
 				//horizontal checks
-				if(myBoard.board[0][j].piece.player == myBoard.board[1][j].piece.player && myBoard.board[1][j].piece.player == myBoard.board[2][j].piece.player) {
+				if(myBoard.pieces[0][j].player == myBoard.pieces[1][j].player && myBoard.pieces[1][j].player == myBoard.pieces[2][j].player) {
 					return true;
 				}
-				if(myBoard.board[1][j].piece.player == myBoard.board[2][j].piece.player && myBoard.board[2][j].piece.player == myBoard.board[3][j].piece.player) {
+				if(myBoard.pieces[1][j].player == myBoard.pieces[2][j].player && myBoard.pieces[2][j].player == myBoard.pieces[3][j].player) {
 					return true;
 				}
 				
 				//vertical checks
-				if(myBoard.board[i][0].piece.player == myBoard.board[i][1].piece.player && myBoard.board[i][1].piece.player == myBoard.board[i][2].piece.player) {
+				if(myBoard.pieces[i][0].player == myBoard.pieces[i][1].player && myBoard.pieces[i][1].player == myBoard.pieces[i][2].player) {
 					return true;
 				}
-				if(myBoard.board[i][1].piece.player == myBoard.board[i][2].piece.player && myBoard.board[i][2].piece.player == myBoard.board[i][3].piece.player) {
+				if(myBoard.pieces[i][1].player == myBoard.pieces[i][2].player && myBoard.pieces[i][2].player == myBoard.pieces[i][3].player) {
 					return true;
 				}
 				
-				//diagonal checks
+				//diagonal checkss
 				if(i != 0 && i != 4 && j != 0 && j != 4) {
 					//diagonal checks \ direction
-					if(myBoard.board[i-1][j+1].piece.player == myBoard.board[i][j].piece.player && myBoard.board[i][j].piece.player == myBoard.board[i+1][j-1].piece.player) {
+					if(myBoard.pieces[i-1][j+1].player == myBoard.pieces[i][j].player && myBoard.pieces[i][j].player == myBoard.pieces[i+1][j-1].player) {
 						return true;
 					}
 					//diagonal checks / direction
-					if(myBoard.board[i-1][j-1].piece.player == myBoard.board[i][j].piece.player && myBoard.board[i][j].piece.player == myBoard.board[i+1][j+1].piece.player) {
+					if(myBoard.pieces[i-1][j-1].player == myBoard.pieces[i][j].player && myBoard.pieces[i][j].player == myBoard.pieces[i+1][j+1].player) {
 						return true;
 					}
 				}		
 			}
 		}
-		myBoard[]
+		return false;
 	}
 	
 }
